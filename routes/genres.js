bc81a69e3687346bc0ac12e3f393476066dd31b8
ExcViral <exc.viral@gmail.com>
2021-01-20 10:08:30 +0530

    Refactored code to use database insted of arrays
    - Updated all the route handlers with async functions to interact with database
    - Implemented CRUD operations for genres API
    	- Functionality to create and save new genre document to the database with two level input validation
    	- Functionality to Fetch all or single genre document from the database
    	- Functionality to Update genre documents in the database
    	- Functionality to Delete genre documents from the database
    - Defined Schema for genres collection
    - Implemented SchemaType options for genres collection

--- routes/genres.js
+++ routes/genres.js
@@ -1,89 +1,144 @@
 const Joi = require('joi')
 const express = require('express')
 const router = express.Router()
+const debug = require('debug')('app:genres')
+const mongoose = require('mongoose')
 
-// Genres Array
-// TODO: Implement Database
-Genres = [
-	{Gid: 1, name: "Action"},
-	{Gid: 2, name: "Adventure"},
-	{Gid: 3, name: "Comedy"},
-	{Gid: 4, name: "Crime"},
-	{Gid: 5, name: "Drama"}
-]
+// check database status, give it one second so that db gets connected
+setTimeout(() => {
+	const state = mongoose.connection.readyState
+	if(state == 0) debug("Database is disconnected")
+	else if(state == 1) debug("Database is connected")
+	else if(state == 2) debug("Database is connecting")
+	else if(state == 3) debug("Database is disconnecting")
+}, 1000)
 
-// EndPoint for getting the list of all Genres
-router.get("/", (req, res) => {
-	res.send(Genres)
+// create schema for genres
+const genreSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 3,
+		trim: true,
+		set: function(s) {
+			let words = s.split(" ")
+			for (let i = 0; i < words.length; i++) {
+			    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+			}
+			return words.join(" ")
+		}
+	}
+})
+
+// compile it into model
+const Genre = mongoose.model("Genre", genreSchema)
+
+// ///////////////////////////////////////////////////////////
+// ROUTE HANDLERS: /api/genres
+// ///////////////////////////////////////////////////////////
+
+// // EndPoint for getting the list of all Genres
+router.get("/", async (req, res) => {
+	debug("GET /api/genres")
+	try {
+		const genres = await Genre.find({})
+		res.send(genres)
+	}
+	catch(ex) {
+		console.log(ex)
+		// send 500 internal server error
+		res.status(500).send("Sorry, we could not process your request!")
+	}
 })
 
 // EndPoint for getting a single Genre
-router.get("/:id", (req, res) => {
-	// check whether the requested id exists
-	const genre = Genres.find(i => i.Gid === parseInt(req.params.id))
-	// if requested id doesnot exist, return 404 not found
-	if (!genre) return res.status(404).send("Requested Genre Doesnot Exist")
-
-	// otherwise return the requested genre
-	res.send(genre)
+router.get("/:id", async (req, res) => {
+	debug(`GET /api/genres/${req.params.id}`)
+	try {
+		const genre = await Genre.findById(req.params.id)
+		// return immediately if id doesnot exist
+		if (!genre) return res.status(404).send("Requested Genre Doesnot Exist")
+		// otherwise return genre to the client 
+		res.send(genre)
+	}
+	catch(ex) {
+		console.log(ex)
+		// send 500 internal server error
+		res.status(500).send("Sorry, we could not process your request")	
+	}
+	// getGenreByID(req.params.id, res)
 })
 
 // EndPoint for creating a new Genre
-router.post("/", (req, res) => {
-	// Generate id for the genre
-	const id = Genres[Genres.length-1].Gid + 1
+router.post("/", async (req, res) => {
+	debug("POST /api/genres")
 	// validate the new Genre Name
 	const {value, error} = validateGenreName(req.body)
 	// Return 400 for invalid input
 	if(error) return res.status(400).send(error.details[0].message)
-	// otherwise create a new genre and add it
-	const newGenre = {
-		Gid: id,
-		name: value.name
+	// otherwise create a genre
+	try{
+		let genre = new Genre({
+			name: value.name
+		})
+		genre = await genre.save()
+		res.send(genre)
 	}
-	Genres.push(newGenre)
-	// By Convention, return the newly added Genre to the client
-	res.send(newGenre)
+	catch(ex) {
+		console.log(ex)
+		// send 500 internal server error
+		res.status(500).send("Sorry, we could not process your request")
+	}
 })
 
 // EndPoint for updating existing Genres
-router.put("/:id", (req, res) => {
-	// check whether the requested id exists
-	const genre = Genres.find(i => i.Gid === parseInt(req.params.id))
-	// if requested id doesnot exist, return 404 not found
-	if (!genre) return res.status(404).send("Requested Genre Doesnot Exist")
-	
-	// validate the requested update name
-	const {value, error} = validateGenreName(req.body)
-	// if invalid input, return 400 bad request
-	if(error) return res.status(400).send(error.details[0].message)
-
-	// otherwise update the Genre
-	genre.name = value.name
-
-	// by convention, send the updated genre to the client
-	res.send(genre)
+router.put("/:id", async (req, res) => {
+	debug(`PUT /api/genres/${req.params.id}`)	
+	try {
+		let genre = await Genre.findById(req.params.id)
+		// return immediately if the input id does not exist
+		if (!genre) return res.status(404).send("Requested Genre Doesnot Exist")
+		// validate the requested update name
+		const {value, error} = validateGenreName(req.body)
+		// if invalid input, return 400 bad request
+		if(error) return res.status(400).send(error.details[0].message)
+		// otherwise update the course and return updated course to client
+		genre.set({
+			name: value.name
+		})
+		genre = await genre.save()
+		res.send(genre)
+	}
+	catch(ex) {
+		console.log(ex)
+		// send 500 internal server error
+		res.status(500).send("Sorry, we could not process your request")		
+	}
 })
 
 // EndPoint for deleting existing Genres
-router.delete("/:id", (req, res) => {
-	// check whether the requested id exists
-	const genre = Genres.find(i => i.Gid === parseInt(req.params.id))
-	// if requested id doesnot exist, return 404 not found
-	if (!genre) return res.status(404).send("Requested Genre Doesnot Exist")
-
-	// otherwise delete the Genre
-	const index = Genres.indexOf(genre)
-	Genres.splice(index, 1)
-	// By convention return the deleted Genre to the client 
-	res.send(genre)
+router.delete("/:id", async (req, res) => {
+	debug(`DELETE /api/genres/${req.params.id}`)
+	try{
+		const course = await Genre.findByIdAndRemove(req.params.id)
+		// if course doesnot exist
+		if (!course) return res.status(404).send("Requested Genre Doesnot Exist")
+		// otherwise return deleted genre to the client
+		res.send(course)
+	}
+	catch(ex) {
+		console.log(ex)
+		// send 500 internal server error
+		res.status(500).send("Sorry, we could not process your request")
+	}
 })
 
 // export the router object
 module.exports = router
 
-
-// Functions
+// ///////////////////////////////////////////////////////////
+// HELPER FUNCTIONS
+// ///////////////////////////////////////////////////////////
 
 // Function to Validate a Genre Name
 // input: Object with key - name and value -string
