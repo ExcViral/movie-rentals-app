3488ff576e076ea19b940e45f3fc1336ab11ce0a
ExcViral <exc.viral@gmail.com>
2021-02-03 12:05:38 +0530

    Added support for authentication and authorization and formatted code using prettify vscode extension

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-const express = require('express')
-const app = express()
-const debug = require('debug')("app:main")
-const config = require('config')
-const mongoose = require('mongoose')
-const Joi = require('joi')
-Joi.objectId = require('joi-objectid')(Joi)
+const express = require('express');
+const app = express();
+const debug = require('debug')('app:main');
+const config = require('config');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+// check if all environment variables are set
+// for json web token private key
+if (!config.get('jwt.pkey')) {
+	console.error('FATAL ERROR: vidly_jwtpkey is not defined');
+	process.exit(1);
+}
 
 // connect to db
-const connStr = config.get("db.connStr")
-debug(`connection string: ${connStr}`)
+const connStr = config.get('db.connStr');
+debug(`connection string: ${connStr}`);
 
-mongoose.connect(connStr, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => { debug("Connected to the database") })
-	.catch((err) => { console.log("Could not connect to mongoDB", err)} )
+mongoose
+	.connect(connStr, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		debug('Connected to the database');
+	})
+	.catch(err => {
+		console.log('Could not connect to mongoDB', err);
+	});
 
 // router imports
-const genresAPI = require("./routes/genres.js")
-const customersAPI = require("./routes/customers.js")
-const moviesAPI = require("./routes/movies.js")
-const rentalAPI = require("./routes/rentals.js")
+const genresAPI = require('./routes/genres.js');
+const customersAPI = require('./routes/customers.js');
+const moviesAPI = require('./routes/movies.js');
+const rentalsAPI = require('./routes/rentals.js');
+const usersAPI = require('./routes/users.js');
+const authAPI = require('./routes/auth.js');
 
-app.use(express.json())
+// middlewares
+app.use(express.json());
 
-// import the generes router module 
-app.use("/api/genres", genresAPI)
-app.use("/api/customers", customersAPI)
-app.use("/api/movies", moviesAPI)
-app.use("/api/rentals", rentalAPI)
-
+// export handling of routes to their respective routers
+app.use('/api/genres', genresAPI);
+app.use('/api/customers', customersAPI);
+app.use('/api/movies', moviesAPI);
+app.use('/api/rentals', rentalsAPI);
+app.use('/api/users', usersAPI);
+app.use('/api/login', authAPI);
 
 // Listen
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-	console.log(`Listening on Port: ${PORT}`)
-})
+	console.log(`Listening on Port: ${PORT}`);
+});
