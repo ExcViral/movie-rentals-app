1f94c6c54af8d231335e8d097b1972b393f42d44
ExcViral <exc.viral@gmail.com>
2021-03-02 17:10:01 +0530

    Refactored `index.js`
    - In `index.js`, eariler we had a lack of separation of concerns. There were too many require statements in our `index.js`.
    - Then we had code for handling and logging errors.
    - Then we had something completely different, like configuration settings.
    - Then we had logic for connecting to the database.
    - Then we set up routes, middlewares etc...
    - All these were different concerns, and mixing them together in a single module is not a good idea.
    
    - Our `index.js` module should only orchestrate these different concerns.
    - So, the details of this orchestration were moved to other modules. For example, setting up routes and connecting to database are separate concerns, so were moved in their own modules.
    
    - We created a new folder named `startup/` where we created new modules: `config.js`, `db.js`, `logging.js`, and `routes.js` for each of these concerns and stored details(code) there.

--- index.js
+++ index.js
@@ -2,83 +2,25 @@ const express = require('express');
 const app = express();
 const winston = require('winston');
 const debug = require('debug')('app:main');
-const config = require('config');
-const mongoose = require('mongoose');
+
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
-winston.add(new winston.transports.File({ filename: 'logFile.log' }));
+// check if all configuration settings are ok
+require('./startup/config.js')();
 
-// check if all environment variables are set
-// for json web token private key
-if (!config.get('jwt.pkey')) {
-	console.error('FATAL ERROR: vidly_jwtpkey is not defined');
-	process.exit(1);
-}
+// setup error logging (should be set up first so that all errors are handled)
+require('./startup/logging.js')();
 
-// Handle Uncaught Exceptions and log them
-process.on('uncaughtException', ex => {
-	winston.log('error', ex.message, ex);
-	process.exit(1);
-});
+// setup and connect to database
+require('./startup/db.js')();
 
-// Handle Uncaught Promise Rejections and log them
-process.on('unhandledRejection', ex => {
-	winston.log('error', ex.message, ex);
-	process.exit(1);
-});
-
-// connect to db
-const connStr = config.get('db.connStr');
-debug(`connection string: ${connStr}`);
-
-mongoose
-	.connect(connStr, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => {
-		debug('Connected to the database');
-	})
-	.catch(err => {
-		console.log('Could not connect to mongoDB', err);
-	});
-
-// middleware imports
-const errorMiddleware = require('./middlewares/error.js');
-
-// router imports
-const genresAPI = require('./routes/genres.js');
-const customersAPI = require('./routes/customers.js');
-const moviesAPI = require('./routes/movies.js');
-const rentalsAPI = require('./routes/rentals.js');
-const usersAPI = require('./routes/users.js');
-const authAPI = require('./routes/auth.js');
-
-// middlewares
-app.use(express.json());
-
-// https://enable-cors.org/server_expressjs.html
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
-
-// export handling of routes to their respective routers
-app.use('/api/genres', genresAPI);
-app.use('/api/customers', customersAPI);
-app.use('/api/movies', moviesAPI);
-app.use('/api/rentals', rentalsAPI);
-app.use('/api/users', usersAPI);
-app.use('/api/login', authAPI);
-// register the error middleware
-app.use(errorMiddleware);
+// setup all routes
+require('./startup/routes.js')(app);
 
 // Listen
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-	console.log(`Listening on Port: ${PORT}`);
+	winston.info(`Listening on Port: ${PORT}`);
 });
